c2f775e5911cef6060b556a6e30a67acd45565ed
HAROON KHALID <105095792+HaroonKhalid222@users.noreply.github.com>
2023-10-03 15:04:21 +0500

    Time: 4 ms (30.92%), Space: 9.1 MB (17.55%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int i = digits.size()-1;
-        for(int j=i;i>=0;j--){
-            if(digits[i]==9){
+        for(int j=i;i>=0;j--)
+        {
+            if(digits[i]==9)
+            {
                 digits[i] = 0;
             }
             else{
                digits[i]++;
                 return digits;
             }
-            i--;
+            i=i-1;
         }
         //now inserting one for the last case where input is [9]
         digits.insert(digits.begin() , 1);
